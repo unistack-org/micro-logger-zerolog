f4c8a9d18c5b596f5311c64072252521820d284c
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-11-10 12:23:56 +0300

    test for change output
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- zerolog_test.go
+++ zerolog_test.go
@@ -1,6 +1,7 @@
 package zerolog
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"os"
@@ -10,6 +11,18 @@ import (
 	"github.com/rs/zerolog"
 	"go.unistack.org/micro/v3/logger"
 )
+
+func TestOutput(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	l := NewLogger(logger.WithOutput(buf))
+	if err := l.Init(); err != nil {
+		t.Fatal(err)
+	}
+	l.Infof(context.TODO(), "test logger name: %s", "name")
+	if !bytes.Contains(buf.Bytes(), []byte(`test logger name`)) {
+		t.Fatalf("log not redirected: %s", buf.Bytes())
+	}
+}
 
 func TestName(t *testing.T) {
 	l := NewLogger()
